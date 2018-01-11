b1d55d8c5c14b1779a2d8026f7fea576ccbe10e1
Chris Swanner and Joel Adams <chris@>
2018-01-11 11:32:05 -0800

    add political parties, js for test results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   function checkAge() {
     var age = parseInt(prompt("How old are you?"));
 
@@ -14,9 +15,35 @@ $(document).ready(function(){
     }
   };
   checkAge();
+
   $('#formOne').submit(function(event) {
     $('#form').hide();
-    alert("The police are on their way. Stay where you are.");
+
   event.preventDefault();
+  });
+
+  var add = function(q1Input, q2Input, q3Input) {
+    return q1Input + q2Input + q3Input;
+  }
+
+  $('#test').submit(function(event) {
+    var q1Input = parseInt($("#q1").val());
+    var q2Input = parseInt($("#q2").val());
+    var q3Input = parseInt($("#q3").val());
+    var result = add(q1Input, q2Input, q3Input);
+    $('#party').hide();
+    if (result <= 4) {
+      $('#rhino').show();
+    } else if (result <=6) {
+      $('#death').show();
+    } else if (result <=8) {
+      $('#canada').show();
+    } else if (result === 9) {
+      $('#police').show();
+      alert("The police are on their way. Stay where you are.");
+    }
+  event.preventDefault();
+
+
   });
 });
