96e6f938980602d4e85a0d9da68dca0c91a987a8
Chris Swanner and Joel Adams <joel@>
2018-01-11 09:22:27 -0800

    add return loop if age is not entered into prompt

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
-  var age = parseInt(prompt("How old are you?"));
+  function checkAge() {
+    var age = parseInt(prompt("How old are you?"));
 
-  if (age < 18) {
-    $('#minors').show();
-  } else if (age === 18) {
-    alert("Congratulations!");
-    $('#adults').show();
-  } else {
-    $('#adults').show();
-  }
+    if (age < 18) {
+      $('#minors').show();
+    } else if(isNaN(age)){
+       if (confirm("Please enter in a number")) checkAge();
+    } else if (age === 18) {
+      alert("Congratulations!");
+      $('#adults').show();
+    } else {
+      $('#adults').show();
+    }
+  };
+  checkAge();
 });
